7e0d871b3b3ad5308d4755270d5caf456f8bb79b
Aliaksandr Lindarenka <siamxiii@gmail.com>
2015-04-27 17:30:03 +0300

    added basic mongodb connection

--- air-ticket/air-ticket-server/server.js
+++ air-ticket/air-ticket-server/server.js
@@ -1,6 +1,10 @@
 ﻿var http = require('http');
+var dataAccessLayer = require('./data-access/data-access.js');
+
 var port = process.env.port || 1337;
 http.createServer(function (req, res) {
-    res.writeHead(200, { 'Content-Type': 'text/plain' });
-    res.end('Bye World\n');
+	res.writeHead(200, { 'Content-Type': 'text/plain' });
+	res.end('Bye World\n');
 }).listen(port);
+
+console.log(dataAccessLayer.Flights.find({}));
