539c0d4189a3caa7ac84b2875389699f40068213
FakaMaka <fakamaka@live.ru>
2015-04-28 10:42:20 +0300

    Add missing files.

---
+++ air-ticket/Gruntfile.js
@@ -0,0 +1,32 @@
+module.exports = function (grunt) {
+
+	// Project configuration.
+	grunt.initConfig({
+		pkg: grunt.file.readJSON('package.json'),
+		copy: {
+			main: {
+				files: [
+				  // includes files within path and its sub-directories 
+				  { expand: true, src: ['air-ticket-server-interface/**'], dest: 'air-ticket-server/custom_modules/' },
+				  { expand: true, src: ['air-ticket-server-interface/**'], dest: 'air-ticket-client/custom_modules/' }
+				]
+			},
+		},
+		watch: {
+			scripts: {
+				files: ['air-ticket-server-interface/**'],
+				tasks: ['copy'],
+				options: {
+					spawn: false,
+				},
+			}
+		}
+	});
+
+	// Load the plugin that provides the "uglify" task.
+	grunt.loadNpmTasks('grunt-contrib-copy');
+	grunt.loadNpmTasks('grunt-contrib-watch');
+
+	// Default task(s).
+	grunt.registerTask('default', ['copy', 'watch']);
+};
