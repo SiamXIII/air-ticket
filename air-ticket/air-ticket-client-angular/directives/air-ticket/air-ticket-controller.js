3a6fa8adfef9a114d27e1cba5a1d88715ce51b45
Aliaksandr Lindarenka <siamxiii@gmail.com>
2015-04-29 19:00:42 +0300

    modified dropboxes

--- air-ticket/air-ticket-client-angular/directives/air-ticket/air-ticket-controller.js
+++ air-ticket/air-ticket-client-angular/directives/air-ticket/air-ticket-controller.js
@@ -1,5 +1,9 @@
 ﻿angular.module('airTicketApp')
 .controller('ticketsCtrl', function ($scope, $http) {
+	$scope.select2options = {
+
+	};
+
 	$http.get("http://localhost:3000/api/0.1.0/tickets").then(function (data) {
 		$scope.tickets = data.data;
 	});
