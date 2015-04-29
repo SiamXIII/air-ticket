9a508db9f9454536422899213db31c8bed306c46
Aliaksandr Lindarenka <siamxiii@gmail.com>
2015-04-29 15:16:04 +0300

    basic search markup

--- air-ticket/air-ticket-client-angular/directives/air-ticket/air-ticket-directive.js
+++
@@ -1,7 +0,0 @@
-﻿angular.module('airTicketApp')
-	.directive('airTicket', function () { 
-		return {
-			templateUrl:'directives/air-ticket//air-ticket.html',
-			controller:'ticketsCtrl'
-		}
-	});
