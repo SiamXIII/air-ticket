33638a004ce0a847345a6ed94fb5aa9c48886126
FakaMaka <fakamaka@live.ru>
2015-04-29 15:22:07 +0300

    Delete all typescript.

--- air-ticket/resolveDependenties.ps1
+++ air-ticket/resolveDependenties.ps1
@@ -2,12 +2,10 @@ npm install;
 
 Push-Location -Path air-ticket-client-angular;
 npm install;
-tsd reinstall -os;
 
 Pop-Location;
 Push-Location -Path air-ticket-server;
 npm install;
-tsd reinstall -os;
 
 Pop-Location;
 grunt;
