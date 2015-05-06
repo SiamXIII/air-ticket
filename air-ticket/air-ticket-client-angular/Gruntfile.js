e9cc84b9b3a6e765c73f39bb8a39d385f2e3bff4
FakaMaka <fakamaka@live.ru>
2015-05-06 11:09:59 +0300

    Add styles for select 2.

--- air-ticket/air-ticket-client-angular/Gruntfile.js
+++ air-ticket/air-ticket-client-angular/Gruntfile.js
@@ -1,47 +1,7 @@
 ﻿module.exports = function (grunt) {
 	grunt.initConfig({
-		pkg: grunt.file.readJSON('package.json'),
-		less: {
-			development: {
-				files: {
-					"build/styles.css": "src/css/styles.less"
-				}
-			}
-		},
-
-		copy: {
-			main: {
-				files: [
-					{
-						expand: true,
-						cwd: 'bower_components/bootstrap/dist/css',
-						src: ['bootstrap.css'],
-						dest: 'build'
-					},
-					{
-						expand: true,
-						cwd: 'bower_components/bootstrap/dist/js',
-						src: ['bootstrap.min.js'],
-						dest: 'build'
-					}
-				]
-			}
-		},
-
-		concat: {
-			dist: {
-				src: ['build/bootstrap.css', 'build/styles.css'],
-				dest: 'build/bundle.css'
-			}
-		},
-
-		clean: ['build/bootstrap.css', 'build/styles.css']
+		pkg: grunt.file.readJSON('package.json')
 	});
 
-	grunt.loadNpmTasks('grunt-contrib-less');
-	grunt.loadNpmTasks('grunt-contrib-copy');
-	grunt.loadNpmTasks('grunt-contrib-concat');
-	grunt.loadNpmTasks('grunt-contrib-clean');
-
-	grunt.registerTask('default', ['less', 'copy', 'concat', 'clean']);
+	grunt.registerTask('default', []);
 };
