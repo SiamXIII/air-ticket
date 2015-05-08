c673ff7715534483bd74bc51dcbf0e22fba38fb8
FakaMaka <fakamaka@live.ru>
2015-05-08 16:37:31 +0300

    Restore flight map.

--- air-ticket/air-ticket-server/flightMap.js
+++ air-ticket/air-ticket-server/flightMap.js
@@ -1,17 +1,78 @@
-﻿var flightsDataAccess = require('./data-access/data-access.js').Flights;
-var locationsDataAccess = require('./data-access/data-access.js').Locations;
-
-var Flight = require('./domain-models/flight.js').Flight;
-var Location = require('./domain-models/location.js').Location;
-
-var FlightMap = function () {
-	flightsDataAccess.find({})
-	.populate('from')
-	.populate('to')
-	.lean(true)
-	.exec(function (err, data) {
-		this.flights = data;
-	});
-}
-
-module.exports = FlightMap;
+﻿﻿ var FlightsMap = (function () {
+    function FlightsMap(flights) {
+        this.flights = flights;
+    }
+    FlightsMap.prototype.getFlightsFrom = function (placeCode) {
+        var resultFlights = [];
+        for (var i = 0; i < this.flights.length; i++) {
+            var flight = this.flights[i];
+            if (flight.from.code === placeCode) {
+                resultFlights.push(flight);
+            }
+        }
+        return resultFlights;
+    };
+    FlightsMap.prototype.getFlightsTo = function (placeCode) {
+        var resultFlights = [];
+        for (var i = 0; i < this.flights.length; i++) {
+            var flight = this.flights[i];
+            if (flight.to.code === placeCode) {
+                resultFlights.push(flight);
+            }
+        }
+        return resultFlights;
+    };
+    FlightsMap.prototype.checkRoute = function (route, query) {
+        if (route.length > 5) {
+            return {
+                stopSearching: true,
+                requiredRoute: false,
+                continueSearching: false
+            };
+        }
+        if (route[0].from.code === query.fromCode && route[route.length - 1].to.code === query.toCode) {
+            return {
+                stopSearching: false,
+                requiredRoute: true,
+                continueSearching: false
+            };
+        }
+        return {
+            stopSearching: false,
+            requiredRoute: false,
+            continueSearching: true
+        };
+    };
+    FlightsMap.prototype.getRoutes = function (query) {
+        var routeStack = [];
+        var fromFlights = this.getFlightsFrom(query.fromCode);
+        while (fromFlights.length > 0) {
+            routeStack.push([fromFlights.shift()]);
+        }
+        var resultRoutes = [];
+        while (routeStack.length > 0) {
+            var route = routeStack.pop();
+            var routeCheckResult = this.checkRoute(route, query);
+            if (routeCheckResult.requiredRoute) {
+                resultRoutes.push(route);
+            }
+            else if (routeCheckResult.stopSearching) {
+                continue;
+            }
+            else if (routeCheckResult.continueSearching) {
+                var nextFlights = this.getFlightsFrom(route[route.length - 1].to.code);
+                while (nextFlights.length > 0) {
+                    var newRoute = route.slice(0); // clone
+                    newRoute.push(nextFlights.shift());
+                    routeStack.push(newRoute);
+                }
+            }
+            else {
+                throw "Unknown routeCheckResult";
+            }
+        }
+        return resultRoutes;
+    };
+    return FlightsMap;
+})();
+module.exports = FlightsMap;
