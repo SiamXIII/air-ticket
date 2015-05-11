602a3bccc5dbc5e75ff2934122a98f521074498e
FakaMaka <fakamaka@live.ru>
2015-05-11 16:39:34 +0300

    move repeated markup to templates.

---
+++ air-ticket/air-ticket-client-angular/app/directives/ticket-list/route/route.js
@@ -0,0 +1,11 @@
+﻿angular.module('airTicketApp')
+.directive('route', function (templatesPath) {
+	return {
+		restrict: 'A',
+		require: '^ticket-list',
+		templateUrl: templatesPath + 'route.html',
+		scope: {
+			route: "="
+		}
+	}
+});
