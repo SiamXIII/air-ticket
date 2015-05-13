2973285ed2bbaf833900226d28d53cd635ecdee7
DzianisPasiukou <d.pasukov@mail.ru>
2015-05-13 17:59:07 +0300

    Added calendar.

---
+++ air-ticket/air-ticket-client-angular/app/directives/calendar/calendar.js
@@ -0,0 +1,33 @@
+﻿angular.module('airTicketApp')
+.directive('datepicker', function () {
+	return {
+		link: function (scope, element, attrs) {
+			element.attr('readonly', true);
+
+			element.datepicker({
+				showOnFocus: false,
+				autoclose: true,
+				todayBtn: "linked",
+				clearBtn: true
+			}).on('hide', function () {
+				element.focus();
+			});
+
+			element.click(function () {
+				element.datepicker('show');
+			});
+
+			element.keypress(function (event) {
+				var keycode = (event.keyCode ? event.keyCode : event.which);
+				if (keycode == '13') {
+					element.datepicker('show');
+				}
+			});
+
+			element.focusout(function () {
+				//element.datepicker('hide');
+			});
+		},
+		restrict: "A"
+	};
+});
