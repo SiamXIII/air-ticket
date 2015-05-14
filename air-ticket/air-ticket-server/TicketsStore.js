8752b5817b18ffb82e65a0df97216857c4eacde1
Aliaksandr Lindarenka <siamxiii@gmail.com>
2015-05-14 17:01:58 +0300

    ticket store fix.

--- air-ticket/air-ticket-server/TicketsStore.js
+++ air-ticket/air-ticket-server/TicketsStore.js
@@ -3,61 +3,61 @@ var locationsDataAccess = require('./data-access/data-access.js').Locations;
 var Entities = require('./domain/Entities.js');
 
 var instance = {
-    getAllLocations: function (callback) {
-        locationsDataAccess.find()
+	getAllLocations: function (callback) {
+		locationsDataAccess.find()
 			.lean(true)
 			.exec(function (err, data) {
-            var locations = [];
-            
-            data.forEach(function (location) {
-                locations.push(new Entities.Location(location.code, location.fullName, "-0500"));
-            });
-            
-            callback(locations);
-        });
-    },
-    
-    getAllCities: function (callback) {
-        instance.getAllLocations(function (data) {
-            var allCities = data.map(function (location) { return location.getCityCode() })
+			var locations = [];
+			
+			data.forEach(function (location) {
+				locations.push(new Entities.Location(location.code, location.fullName, location.timeZoneOffset));
+			});
+			
+			callback(locations);
+		});
+	},
+	
+	getAllCities: function (callback) {
+		instance.getAllLocations(function (data) {
+			var allCities = data.map(function (location) { return location.getCityCode() })
 				.sort()
 				.filter(function (city, index, arr) {
-                return index === 0 ||
+				return index === 0 ||
 						city !== arr[index - 1];
-            });
-            
-            callback(allCities);
-        });
-    },
-    
-    getAllFlights: function (callback) {
-        flightsDataAccess.find()
+			});
+			
+			callback(allCities);
+		});
+	},
+	
+	getAllFlights: function (callback) {
+		flightsDataAccess.find()
 			.populate('_from')
 			.populate('_to')
 			.lean(true)
 			.exec(function (err, data) {
-            var flights = [];
-            
-            data.forEach(function (flight) {
-                flights.push(new Entities.Flight(
-                    new Entities.Location(flight._from.code, flight._from.fullName, flight._from.city), 
-					new Entities.Location(flight._to.code, flight._to.fullName, flight._to.city), 
+			var flights = [];
+			
+			data.forEach(function (flight) {
+				flights.push(new Entities.Flight(
+					new Entities.Location(flight._from.code, flight._from.fullName, flight._from.timeZoneOffset), 
+					new Entities.Location(flight._to.code, flight._to.fullName, flight._to.timeZoneOffset), 
 					flight.departureTime, flight.arrivalTime, flight.flightCode, flight.vendor, flight.price));
-            })
-            
-            callback(flights);
-        })
-    },
-    
-    getAllCities: function (callback) {
-        locationsDataAccess.find()
+			})
+			
+			callback(flights);
+		})
+	},
+	
+	getAllCities: function (callback) {
+		locationsDataAccess.find()
 			.distinct('city')
 			.exec(function (err, data) {
-            callback(data);
-        })
-    }
+			callback(data);
+		})
+	}
 }
 
 module.exports = function () {
-    return instance;
+	return instance;
 }
