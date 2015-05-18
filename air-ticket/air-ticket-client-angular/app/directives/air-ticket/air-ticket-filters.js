723d9a700f94d5fa0d4890c1a984fbae6e6acb8e
FakaMaka <fakamaka@live.ru>
2015-05-18 16:04:15 +0300

    Implement "duration" filter.

--- air-ticket/air-ticket-client-angular/app/directives/air-ticket/air-ticket-filters.js
+++ air-ticket/air-ticket-client-angular/app/directives/air-ticket/air-ticket-filters.js
@@ -3,4 +3,17 @@
 		return function(item, correction) {
 			return moment(item).utc().add(correction, 'm').format('HH:mm');
 		}
+	}).filter('duration', function() {
+		return function(miliseconds) {
+			var milisecondsInHour = 1000 * 60 * 60;
+			var milisecondsInMinute = 1000 * 60;
+			var hours = miliseconds / milisecondsInHour;
+			var minutes = (miliseconds - hours * milisecondsInHour) / milisecondsInMinute;
+			minutes = minutes.toString();
+			if (minutes.length === 1) {
+				minutes = "0" + minutes;
+			}
+			var duration = hours + ":" + minutes;
+			return duration;
+		}
 	});
