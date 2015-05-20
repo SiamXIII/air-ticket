1fc2026966291d10162d26caf44b7723867ac64a
DzianisPasiukou <d.pasukov@mail.ru>
2015-05-20 09:58:41 +0300

    Added ignored files.

---
+++ air-ticket/air-ticket-client-angular/app/directives/validation/not-repeat.js
@@ -0,0 +1,21 @@
+﻿angular.module('airTicketApp')
+.directive('notRepeat', ['$parse', function ($parse) {
+	return {
+		restrict: "A",
+		require: "ngModel",
+		scope: {
+			ngModel: '='
+		},
+		link: function ($scope, element, attributes, ngModel) {
+			ngModel.$validators.notRepeat = function (modelValue) {
+				return modelValue != $parse(attributes.notRepeat)($scope);
+			}
+
+			$scope.$watch('ngModel', function (value) {
+				var state = ngModel.$validators.notRepeat(attributes.ngModel);
+
+				ngModel.$setValidity("notRepeat", state);
+			});
+		}
+	}
+}]);
---
+++ air-ticket/air-ticket-client-angular/app/directives/validation/passengers-required.js
@@ -0,0 +1,35 @@
+﻿angular.module('airTicketApp')
+.directive('passengersRequired', function () {
+	return {
+		restrict: "A",
+		require: "ngModel",
+		scope: {
+			ngModel: '='
+		},
+		link: function ($scope, element, attributes, ngModel) {
+
+			ngModel.$validators.passengersRequired = function (passengers) {
+				var summ = 0;
+
+				for (var passenger in passengers) {
+
+					if ((passengers[passenger]) !== undefined && (parseInt(passengers[passenger]) != NaN))
+					{
+						summ += passengers[passenger];
+					}
+					else {
+						return true;
+					}
+				}
+
+				return summ > 0;
+			}
+
+			$scope.$watch('ngModel', function (value) {
+				var state = ngModel.$validators.passengersRequired(value);
+
+				ngModel.$setValidity("passengersRequired", state);
+			}, true);
+		}
+	}
+});
