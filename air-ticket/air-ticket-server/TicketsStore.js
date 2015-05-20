9955a2ffe2e3ed067248d1e609b387ce5f60c96f
DzianisPasiukou <d.pasukov@mail.ru>
2015-05-20 17:55:16 +0300

    Fixed mistakes on ticket store.

--- air-ticket/air-ticket-server/TicketsStore.js
+++ air-ticket/air-ticket-server/TicketsStore.js
@@ -3,61 +3,62 @@ var locationsDataAccess = require('./data-access/data-access.js').Locations;
 var Entities = require('./domain/Entities.js');
 
 var instance = {
-	getAllLocations: function (callback) {
-		locationsDataAccess.find()
+    getAllLocations: function (callback) {
+        locationsDataAccess.find()
 			.lean(true)
 			.exec(function (err, data) {
-			var locations = [];
-			
-			data.forEach(function (location) {
-				locations.push(new Entities.Location(location.code, location.fullName, location.timeZoneOffset * 60));
-			});
-			
-			callback(locations);
-		});
-	},
-	
-	getAllCities: function (callback) {
-		instance.getAllLocations(function (data) {
-			var allCities = data.map(function (location) { return location.getCityCode() })
+            var locations = [];
+            
+            data.forEach(function (location) {
+                locations.push(new Entities.Location(location.code, location.fullName, location.timeZoneOffset * 60));
+            });
+            
+            callback(locations);
+        });
+    },
+    
+    getAllCities: function (callback) {
+        instance.getAllLocations(function (data) {
+            var allCities = data.map(function (location) { return location.getCityCode() })
 				.sort()
 				.filter(function (city, index, arr) {
-				return index === 0 ||
+                return index === 0 ||
 						city !== arr[index - 1];
-			});
-			
-			callback(allCities);
-		});
-	},
-	
-	getAllFlights: function (callback) {
-		flightsDataAccess.find()
+            });
+            
+            callback(allCities);
+        });
+    },
+    
+    getAllFlights: function (callback) {
+        flightsDataAccess.find()
 			.populate('_from')
 			.populate('_to')
 			.lean(true)
-			.exec(function(err, data) {
-				var flights = [];
-
-			data.forEach(function(flight) {
-				flights.push(new Entities.Flight(
-					new Entities.Location(flight._from.code, flight._from.fullName, flight._from.timeZoneOffset * 60),
-					new Entities.Location(flight._to.code, flight._to.fullName, flight._to.timeZoneOffset * 60),
-					flight.departureTime, flight.arrivalTime, flight.flightCode, flight.vendor, flight.price));
-			});
-
-				callback(flights);
-			});
-	},
-	
-	getAllCities: function (callback) {
-		locationsDataAccess.find()
+			.exec(function (err, data) {
+            var flights = [];
+            
+            data.forEach(function (flight) {
+                flights.push(new Entities.Flight(
+                    new Entities.Route(
+                        new Entities.Location(flight._from.code, flight._from.fullName, flight._from.timeZoneOffset * 60),
+                         new Entities.Location(flight._to.code, flight._to.fullName, flight._to.timeZoneOffset * 60)),
+					flight.departureTime, flight.flightCode, flight.vendor, flight.price));
+            });
+            
+            callback(flights);
+        });
+    },
+    
+    getAllCities: function (callback) {
+        locationsDataAccess.find()
 			.distinct('city')
-			.exec(function(err, data) {
-				callback(data);
-			});
-	}
+			.exec(function (err, data) {
+            callback(data);
+        });
+    }
 }
 
 module.exports = function () {
-	return instance;
+    return instance;
 }
