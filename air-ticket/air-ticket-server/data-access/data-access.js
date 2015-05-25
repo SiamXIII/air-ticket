bcd28959c01f765954764a34bce65591fccc0475
Aliaksandr Lindarenka <siamxiii@gmail.com>
2015-05-25 11:53:25 +0300

    Swithed DB.

--- air-ticket/air-ticket-server/data-access/data-access.js
+++ air-ticket/air-ticket-server/data-access/data-access.js
@@ -3,6 +3,9 @@ var config = require('../config/config.js');
 
 var Location = require('./models/location.js');
 var Flight = require('./models/flight.js');
+var Airport = require('./models/airport.js');
+var Airline = require('./models/airline.js');
+var Route= require('./models/route.js');
 
 mongoose.connect(config.get('mongoose:uri'));
 
@@ -10,3 +13,6 @@ var db = mongoose.connection;
 
 module.exports.Locations = mongoose.model('Location', Location);
 module.exports.Flights = mongoose.model('Flight', Flight);
+module.exports.Airports = mongoose.model('Airport', Airport);
+module.exports.Airlines = mongoose.model('Airline', Airline);
+module.exports.Routes = mongoose.model('Route', Route);
